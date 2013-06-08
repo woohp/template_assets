656e5b7b7ac65a0f1c461c46053693de37b9ae47
Hui Peng Hu <woohp135@gmail.com>
2013-06-08 00:19:00 -0700

    id does not include any file extension. use rails caching

--- lib/template_assets/template_assets.rb
+++ lib/template_assets/template_assets.rb
@@ -1,20 +1,36 @@
 module TemplateAssets
   module ViewHelpers
     def template_assets
+      if Rails.cache.exist? '_asset_templates'
+        return Rails.cache.read '_asset_templates'
+      end
+
       assets_dir_length = Rails.root.join("app", "assets", "templates").to_s.length + 1
 
       results = []
       Dir.glob Rails.root.join("app", "assets", "templates", '**', "*html*") do |f|
         f = f.to_s
+
+        # render
         template = Tilt.new(f).render()
 
+        # create the id
+        f = f[assets_dir_length..-1]
         ext = File.extname(f)
-        f = f[assets_dir_length, f.length - assets_dir_length - ext.length]
+        while ext.length > 0
+          f = f[0..-ext.length-1]
+          ext = File.extname(f)
+        end
 
         results << "<script type='text/ng-template' id='#{f}'>#{template}</script>".html_safe
       end
 
-      safe_join(results)
+      output = safe_join(results)
+
+      # cache
+      Rails.cache.write '_assets_templates', output
+
+      return output
     end
   end
 end
